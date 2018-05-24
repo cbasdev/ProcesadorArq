606135f91586f8e6549751cb95a20889e8acc72d
Sebastian <33915497+SebasttianVelez@users.noreply.github.com>
2018-05-24 10:53:14 -0500

    Add files via upload

--- Module-3/DataPath/ControlUnit.vhd
+++ Module-3/DataPath/ControlUnit.vhd
@@ -7,7 +7,17 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity ControlUnit is
     Port ( OP : in  STD_LOGIC_VECTOR (1 downto 0);
            OP3 : in  STD_LOGIC_VECTOR (5 downto 0);
-           OP_Alu : out  STD_LOGIC_VECTOR (5 downto 0));
+			  icc: in STD_LOGIC_VECTOR(3 downto 0);
+			  Cond : 	in STD_LOGIC_VECTOR(3 downto 0);
+			  OP2 : in STD_LOGIC_VECTOR(2 downto 0);
+			  We : out STD_LOGIC;
+			  WeDM : out STD_LOGIC;
+			  RfSource : out STD_LOGIC_VECTOR(1 downto 0);
+			  RfDest : out STD_LOGIC;
+			  PCSource : out STD_LOGIC_VECTOR(1 downto 0);
+			  OP_Alu : out  STD_LOGIC_VECTOR (5 downto 0));
+			  
+			  
 end ControlUnit;
 ----------------------------------------------------------------------------------
 
